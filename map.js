d652b3a37602d6ced214b44e4738a54fe70afb00
Camille Tran <Ctt011@ucsd.edu>
2025-05-17 22:25:50 -0700

    Compute traffic stats for each station

--- map.js
+++ map.js
@@ -86,7 +86,30 @@ map.on('load', async () => {
   }
 );
 
+
 console.log('Loaded trips:', trips.slice(0, 5)); // optional debug
+
+
+const departures = d3.rollup(
+  trips,
+  (v) => v.length,
+  (d) => d.start_station_id
+);
+
+const arrivals = d3.rollup(
+  trips,
+  (v) => v.length,
+  (d) => d.end_station_id
+);
+
+stations = stations.map((station) => {
+  const id = station.short_name;
+  station.arrivals = arrivals.get(id) ?? 0;
+  station.departures = departures.get(id) ?? 0;
+  station.totalTraffic = station.arrivals + station.departures;
+  return station;
+});
+
 
 
   // Append circles to SVG overlay
