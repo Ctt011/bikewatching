250fd3ade22e57d3651159484feeecabd191afb6
Camille Tran <Ctt011@ucsd.edu>
2025-05-17 22:13:47 -0700

    Add initial Mapbox map setup

--- map.js
+++ map.js
@@ -3,3 +3,17 @@ import mapboxgl from 'https://cdn.jsdelivr.net/npm/mapbox-gl@2.15.0/+esm';
 
 // Check that Mapbox GL JS is loaded
 console.log('Mapbox GL JS Loaded:', mapboxgl);
+
+// Set your Mapbox access token
+mapboxgl.accessToken = 'pk.eyJ1IjoiY3R0MDExIiwiYSI6ImNtYXQ3M2J4djBqNHoyd29rYWdsMDB4YzcifQ.Np_wx7rHELKIWAVF6XljsA';
+
+// Create the map
+const map = new mapboxgl.Map({
+  container: 'map', // ID of the div in index.html
+  style: 'mapbox://styles/mapbox/streets-v12',
+  center: [-71.09415, 42.36027], // [longitude, latitude] - Boston/Cambridge
+  zoom: 12,
+  minZoom: 5,
+  maxZoom: 18,
+});
+
